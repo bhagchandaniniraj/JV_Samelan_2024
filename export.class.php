9c3ca1a2f255df5ea16faccd84c0a7813702c952
bhagchandaniniraj <bhagchandani.niraj@gmail.com>
2024-11-04 09:47:43 +0530

    Final Changes done

--- export.class.php
+++ export.class.php
@@ -62,7 +62,7 @@ class MYPDF extends TCPDF {
     public function ColoredTable($header,$data) {
         
         // Colors, line width and bold font
-        $this->SetFillColor(255, 0, 0);
+        $this->SetFillColor(3, 121, 132);
         $this->SetTextColor(255);
         $this->SetDrawColor(128, 0, 0);
         $this->SetLineWidth(0.3);
